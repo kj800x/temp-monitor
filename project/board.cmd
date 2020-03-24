ac6a621ac16532b56711d282c01e51b8632de154
Kevin Johnson <kevin@coolkev.com>
2020-03-24 16:28:42 -0400

    initial

---
+++ project/board.cmd
@@ -0,0 +1,172 @@
+# Pin name action command file
+
+# Start of element J1
+ChangePinName(J1, 3, switch)
+ChangePinName(J1, 2, barrel)
+ChangePinName(J1, 1, center)
+
+# Start of element D4
+ChangePinName(D4, 2, 2)
+ChangePinName(D4, 1, 1)
+
+# Start of element M2
+ChangePinName(M2, 4, -)
+ChangePinName(M2, 3, +)
+ChangePinName(M2, 2, -)
+ChangePinName(M2, 1, +)
+ChangePinName(M2, 6, -)
+ChangePinName(M2, 5, +)
+ChangePinName(M2, 8, -)
+ChangePinName(M2, 7, +)
+
+# Start of element D3
+ChangePinName(D3, 2, 2)
+ChangePinName(D3, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element M1
+ChangePinName(M1, 6, -)
+ChangePinName(M1, 5, +)
+ChangePinName(M1, 8, -)
+ChangePinName(M1, 7, +)
+ChangePinName(M1, 4, -)
+ChangePinName(M1, 3, +)
+ChangePinName(M1, 2, -)
+ChangePinName(M1, 1, +)
+
+# Start of element Q3
+ChangePinName(Q3, B, B)
+ChangePinName(Q3, E, E)
+ChangePinName(Q3, C, C)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element Q4
+ChangePinName(Q4, B, B)
+ChangePinName(Q4, E, E)
+ChangePinName(Q4, C, C)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element Q2
+ChangePinName(Q2, B, B)
+ChangePinName(Q2, E, E)
+ChangePinName(Q2, C, C)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, B, B)
+ChangePinName(Q1, E, E)
+ChangePinName(Q1, C, C)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 146, GPIO_71)
+ChangePinName(U1, 145, GPIO_70)
+ChangePinName(U1, 144, GPIO_73)
+ChangePinName(U1, 143, GPIO_72)
+ChangePinName(U1, 142, GPIO_75)
+ChangePinName(U1, 141, GPIO_74)
+ChangePinName(U1, 140, GPIO_77)
+ChangePinName(U1, 139, GPIO_76)
+ChangePinName(U1, 138, GPIO_79)
+ChangePinName(U1, 137, GPIO_78)
+ChangePinName(U1, 136, GPIO_80)
+ChangePinName(U1, 135, GPIO_8)
+ChangePinName(U1, 134, GPIO_81)
+ChangePinName(U1, 133, GPIO_9)
+ChangePinName(U1, 132, GPIO_11)
+ChangePinName(U1, 131, GPIO_10)
+ChangePinName(U1, 130, GPIO_89)
+ChangePinName(U1, 129, GPIO_87)
+ChangePinName(U1, 128, GPIO_88)
+ChangePinName(U1, 127, GPIO_86)
+ChangePinName(U1, 126, GPIO_61)
+ChangePinName(U1, 125, GPIO_32)
+ChangePinName(U1, 124, GPIO_33)
+ChangePinName(U1, 123, GPIO_36)
+ChangePinName(U1, 122, GPIO_37)
+ChangePinName(U1, 121, GPIO_62)
+ChangePinName(U1, 120, GPIO_63)
+ChangePinName(U1, 119, GPIO_22)
+ChangePinName(U1, 118, GPIO_65)
+ChangePinName(U1, 117, GPIO_27)
+ChangePinName(U1, 116, GPIO_46)
+ChangePinName(U1, 115, GPIO_47)
+ChangePinName(U1, 114, GPIO_26)
+ChangePinName(U1, 113, GPIO_23)
+ChangePinName(U1, 112, GPIO_44)
+ChangePinName(U1, 111, GPIO_45)
+ChangePinName(U1, 110, GPIO_68)
+ChangePinName(U1, 109, GPIO_69)
+ChangePinName(U1, 108, GPIO_67)
+ChangePinName(U1, 107, GPIO_66)
+ChangePinName(U1, 106, GPIO_35)
+ChangePinName(U1, 105, GPIO_34)
+ChangePinName(U1, 104, GPIO_39)
+ChangePinName(U1, 103, GPIO_38)
+ChangePinName(U1, 102, DGND)
+ChangePinName(U1, 101, DGND)
+ChangePinName(U1, 46, DGND)
+ChangePinName(U1, 45, DGND)
+ChangePinName(U1, 44, DGND)
+ChangePinName(U1, 43, DGND)
+ChangePinName(U1, 42, GPIO_7)
+ChangePinName(U1, 41, GPIO_20)
+ChangePinName(U1, 40, AIN1)
+ChangePinName(U1, 39, AIN0)
+ChangePinName(U1, 38, AIN3)
+ChangePinName(U1, 37, AIN2)
+ChangePinName(U1, 36, AIN5)
+ChangePinName(U1, 35, AIN6)
+ChangePinName(U1, 34, GNDA_ADC)
+ChangePinName(U1, 33, AIN4)
+ChangePinName(U1, 32, VDD_ADC)
+ChangePinName(U1, 31, GPIO_120)
+ChangePinName(U1, 30, GPIO_122)
+ChangePinName(U1, 29, GPIO_121)
+ChangePinName(U1, 28, GPIO_123)
+ChangePinName(U1, 27, GPIO_125)
+ChangePinName(U1, 26, GPIO_14)
+ChangePinName(U1, 25, GPIO_117)
+ChangePinName(U1, 24, GPIO_15)
+ChangePinName(U1, 23, GPIO_49)
+ChangePinName(U1, 22, GPIO_2)
+ChangePinName(U1, 21, GPIO_3)
+ChangePinName(U1, 20, I2C2_SDA)
+ChangePinName(U1, 19, I2C2_SCL)
+ChangePinName(U1, 18, GPIO_5)
+ChangePinName(U1, 17, GPIO_4)
+ChangePinName(U1, 16, GPIO_51)
+ChangePinName(U1, 15, GPIO_48)
+ChangePinName(U1, 14, GPIO_40)
+ChangePinName(U1, 13, GPIO_31)
+ChangePinName(U1, 12, GPIO_60)
+ChangePinName(U1, 11, GPIO_30)
+ChangePinName(U1, 10, SYS_RESETn)
+ChangePinName(U1, 9, PWR_BUT)
+ChangePinName(U1, 8, SYS_5V)
+ChangePinName(U1, 7, SYS_5V)
+ChangePinName(U1, 6, VDD_5V)
+ChangePinName(U1, 5, VDD_5V)
+ChangePinName(U1, 4, VDD_3V3)
+ChangePinName(U1, 3, VDD_3V3)
+ChangePinName(U1, 2, DGND)
+ChangePinName(U1, 1, DGND)
---
+++ project/packages/BARREL.fp
@@ -0,0 +1,15 @@
+
+Element["" "" "" "" 2500.00mil 60.7000mm 0.0000 0.0000 0 100 ""]
+(
+	Pin[-4.7000mm 3.0000mm 60.00mil 20.00mil 66.00mil 35.00mil "" "3" ""]
+	Pin[0.0000 6.0000mm 60.00mil 20.00mil 66.00mil 35.00mil "" "1" ""]
+	Pin[0.0000 0.0000 60.00mil 20.00mil 66.00mil 35.00mil "" "2" ""]
+	ElementLine [4.3000mm -6.4000mm -4.3000mm -6.4000mm 25.00mil]
+	ElementLine [4.3000mm -6.9000mm -4.3000mm -6.9000mm 25.00mil]
+	ElementLine [4.3000mm -7.4000mm -4.2000mm -7.4000mm 25.00mil]
+	ElementLine [4.5000mm 6.8000mm -4.5000mm 6.8000mm 10.00mil]
+	ElementLine [4.5000mm -7.7000mm 4.5000mm 6.8000mm 10.00mil]
+	ElementLine [-4.5000mm -7.7000mm 4.5000mm -7.7000mm 10.00mil]
+	ElementLine [-4.5000mm -7.7000mm -4.5000mm 6.8000mm 10.00mil]
+
+	)
