113eec963b6b2b13c9947017abf3aa95ee950055
Kevin Johnson <kevin@coolkev.com>
2020-11-15 21:42:52 +0000

    Motor fixes

--- server/src/motor.js
+++ server/src/motor.js
@@ -7,7 +7,7 @@ const PWM_FREQ = 20000;
 const MOTOR_PIN = "P9_14";
 let value = 0;
 
-function pwmVal = val => val === 0 ? 0 : 0.8 * val + 0.2;
+const pwmVal = val => val === 0 ? 0 : 0.8 * val + 0.2;
 
 if (process.env.MOTOR_ENABLED) {
   setMotor = (val) => {
@@ -15,7 +15,7 @@ if (process.env.MOTOR_ENABLED) {
       return;
     }
     value = val;
-    bonescript.analogWrite(MOTOR_PIN, value, PWM_FREQ, () => {});
+    bonescript.analogWrite(MOTOR_PIN, pwmVal(value), PWM_FREQ, () => {});
   };
 } else {
   console.log("info: motor not enabled");
