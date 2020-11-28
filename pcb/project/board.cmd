ce61b0473da7d011b798f7082c476fc61d68685f
Kevin Johnson <kevin@coolkev.com>
2020-11-28 14:59:51 -0500

    gsch2pcb

--- pcb/project/board.cmd
+++ pcb/project/board.cmd
@@ -1,58 +1,29 @@
 # Pin name action command file
 
-# Start of element CONN1
-ChangePinName(CONN1, 9, 9)
-ChangePinName(CONN1, 10, 10)
-ChangePinName(CONN1, 7, 7)
-ChangePinName(CONN1, 5, 5)
-ChangePinName(CONN1, 3, 3)
-ChangePinName(CONN1, 1, 1)
-ChangePinName(CONN1, 8, 8)
-ChangePinName(CONN1, 6, 6)
-ChangePinName(CONN1, 4, 4)
-ChangePinName(CONN1, 2, 2)
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
 
-# Start of element U2
-ChangePinName(U2, 6, EOC)
-ChangePinName(U2, 3, GND)
-ChangePinName(U2, 2, 3V)
-ChangePinName(U2, 1, VIN)
-ChangePinName(U2, 7, RST)
-ChangePinName(U2, 5, SDA)
-ChangePinName(U2, 4, SCL)
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
 
-# Start of element Q1
-ChangePinName(Q1, 1, B)
-ChangePinName(Q1, 3, E)
-ChangePinName(Q1, 2, C)
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
 
 # Start of element Q2
 ChangePinName(Q2, 1, B)
 ChangePinName(Q2, 3, E)
 ChangePinName(Q2, 2, C)
-
-# Start of element J1
-ChangePinName(J1, 3, switch)
-ChangePinName(J1, 2, barrel)
-ChangePinName(J1, 1, center)
-
-# Start of element D1
-ChangePinName(D1, 2, 2)
-ChangePinName(D1, 1, 1)
-
-# Start of element M1
-ChangePinName(M1, 4, -)
-ChangePinName(M1, 3, +)
-ChangePinName(M1, 2, -)
-ChangePinName(M1, 1, +)
-ChangePinName(M1, 6, -)
-ChangePinName(M1, 5, +)
-ChangePinName(M1, 8, -)
-ChangePinName(M1, 7, +)
-
-# Start of element R1
-ChangePinName(R1, 1, 1)
-ChangePinName(R1, 2, 2)
 
 # Start of element R2
 ChangePinName(R2, 1, 1)
@@ -61,6 +32,39 @@ ChangePinName(R2, 2, 2)
 # Start of element D2
 ChangePinName(D2, 2, 2)
 ChangePinName(D2, 1, 1)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, 1, B)
+ChangePinName(Q1, 3, E)
+ChangePinName(Q1, 2, C)
 
 # Start of element U1
 ChangePinName(U1, 146, GPIO_71)
@@ -74,9 +78,9 @@ ChangePinName(U1, 139, GPIO_76)
 ChangePinName(U1, 138, GPIO_79)
 ChangePinName(U1, 137, GPIO_78)
 ChangePinName(U1, 136, GPIO_80)
-ChangePinName(U1, 135, GPIO_8)
+ChangePinName(U1, 135, GPIO_08)
 ChangePinName(U1, 134, GPIO_81)
-ChangePinName(U1, 133, GPIO_9)
+ChangePinName(U1, 133, GPIO_09)
 ChangePinName(U1, 132, GPIO_11)
 ChangePinName(U1, 131, GPIO_10)
 ChangePinName(U1, 130, GPIO_89)
@@ -113,7 +117,7 @@ ChangePinName(U1, 46, DGND)
 ChangePinName(U1, 45, DGND)
 ChangePinName(U1, 44, DGND)
 ChangePinName(U1, 43, DGND)
-ChangePinName(U1, 42, GPIO_7)
+ChangePinName(U1, 42, GPIO_07)
 ChangePinName(U1, 41, GPIO_20)
 ChangePinName(U1, 40, AIN1)
 ChangePinName(U1, 39, AIN0)
@@ -124,28 +128,28 @@ ChangePinName(U1, 35, AIN6)
 ChangePinName(U1, 34, GNDA_ADC)
 ChangePinName(U1, 33, AIN4)
 ChangePinName(U1, 32, VDD_ADC)
-ChangePinName(U1, 31, GPIO_120)
+ChangePinName(U1, 31, PWM0A)
 ChangePinName(U1, 30, GPIO_122)
-ChangePinName(U1, 29, GPIO_121)
+ChangePinName(U1, 29, PWM0B)
 ChangePinName(U1, 28, GPIO_123)
 ChangePinName(U1, 27, GPIO_125)
 ChangePinName(U1, 26, GPIO_14)
 ChangePinName(U1, 25, GPIO_117)
 ChangePinName(U1, 24, GPIO_15)
 ChangePinName(U1, 23, GPIO_49)
-ChangePinName(U1, 22, GPIO_2)
-ChangePinName(U1, 21, GPIO_3)
+ChangePinName(U1, 22, PWM0A)
+ChangePinName(U1, 21, PWM0B)
 ChangePinName(U1, 20, I2C2_SDA)
 ChangePinName(U1, 19, I2C2_SCL)
-ChangePinName(U1, 18, GPIO_5)
-ChangePinName(U1, 17, GPIO_4)
-ChangePinName(U1, 16, GPIO_51)
+ChangePinName(U1, 18, SPI0_DI)
+ChangePinName(U1, 17, SPI0_CS0)
+ChangePinName(U1, 16, PWM1B)
 ChangePinName(U1, 15, GPIO_48)
-ChangePinName(U1, 14, GPIO_40)
+ChangePinName(U1, 14, PWM1A)
 ChangePinName(U1, 13, GPIO_31)
 ChangePinName(U1, 12, GPIO_60)
 ChangePinName(U1, 11, GPIO_30)
-ChangePinName(U1, 10, SYS_RESETn)
+ChangePinName(U1, 10, SYS_RESET)
 ChangePinName(U1, 9, PWR_BUT)
 ChangePinName(U1, 8, SYS_5V)
 ChangePinName(U1, 7, SYS_5V)
@@ -155,3 +159,47 @@ ChangePinName(U1, 4, VDD_3V3)
 ChangePinName(U1, 3, VDD_3V3)
 ChangePinName(U1, 2, DGND)
 ChangePinName(U1, 1, DGND)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 9, 9)
+ChangePinName(CONN1, 10, 10)
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 6, EOC)
+ChangePinName(U2, 3, GND)
+ChangePinName(U2, 2, 3V)
+ChangePinName(U2, 1, VIN)
+ChangePinName(U2, 7, RST)
+ChangePinName(U2, 5, SDA)
+ChangePinName(U2, 4, SCL)
+
+# Start of element J1
+ChangePinName(J1, 3, switch)
+ChangePinName(J1, 2, barrel)
+ChangePinName(J1, 1, center)
+
+# Start of element M1
+ChangePinName(M1, 8, -)
+ChangePinName(M1, 7, +)
+ChangePinName(M1, 6, -)
+ChangePinName(M1, 5, +)
+ChangePinName(M1, 4, -)
+ChangePinName(M1, 3, +)
+ChangePinName(M1, 2, -)
+ChangePinName(M1, 1, +)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
