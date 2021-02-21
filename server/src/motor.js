8652bfb295b370e829e7270629f27018b861a00b
Kevin Johnson <kevin@coolkev.com>
2021-02-21 02:23:12 -0500

    Upgrade schematic

--- server/src/motor.js
+++ server/src/motor.js
@@ -4,7 +4,7 @@ import bonescript from "bonescript";
 export let setMotor;
 
 const PWM_FREQ = 20000;
-const MOTOR_PIN = "P9_14";
+const MOTOR_PIN = "P9_31";
 let value = 0;
 
 const pwmVal = val => val === 0 ? 0 : 0.8 * val + 0.2;
